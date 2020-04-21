69ef06f3909af8a3e354f888d5381fa979f0ed86
caoanle13 <caoan.le15@imperial.ac.uk>
2020-04-21 19:25:06 +0200

    Updates

---
+++ precache-manifest.1e0b3188ed1dad30dd6674ff22a3037e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a569e8cf9132d93203b38c78389a123",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "6d043e79b38931f03044",
+    "url": "/covid19-tracker/static/css/main.b4c56e08.chunk.css"
+  },
+  {
+    "revision": "7316427ffe7a857f8e7b",
+    "url": "/covid19-tracker/static/js/2.3f606b37.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.3f606b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d043e79b38931f03044",
+    "url": "/covid19-tracker/static/js/main.0b15c890.chunk.js"
+  },
+  {
+    "revision": "998ce9c596a8398666c1",
+    "url": "/covid19-tracker/static/js/runtime-main.75f331d9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
